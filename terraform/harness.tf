7d01e2ea140ce784b75699695c60543d856c791a
Chris Storz <83615348+chrisjws-harness@users.noreply.github.com>
2025-05-21 14:50:10 -0700

    Update harness.tf

--- terraform/harness.tf
+++ terraform/harness.tf
@@ -2,7 +2,7 @@ terraform {
     required_providers {  
         harness = {  
             source = "harness/harness"  
-            version = "0.30.2"
+            version = "0.37.4"
         }  
     }  
 }
