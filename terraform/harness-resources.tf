7494071aaa9943f5c589fde9783ca540405211fd
Chris Storz <chris.storz@harness.io>
2025-05-21 14:48:35 -0700

    fix

--- terraform/harness-resources.tf
+++ terraform/harness-resources.tf
@@ -1,5 +1,5 @@
 resource "harness_platform_project" "project" {  
     name      = var.project_name 
-    identifier = local.project_name  
+    identifier = var.project_name  
     org_id    = "default"  
 }
